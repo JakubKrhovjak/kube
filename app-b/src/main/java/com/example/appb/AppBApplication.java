50297fa6c5555415872d26302e4da2b1d47c513f
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2022-03-26 13:13:09 +0100

    app b

--- app-b/src/main/java/com/example/appb/AppBApplication.java
+++ app-b/src/main/java/com/example/appb/AppBApplication.java
@@ -2,13 +2,21 @@ package com.example.appb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 @SpringBootApplication
 public class AppBApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppBApplication.class, args);
+    }
+
+    @GetMapping
+    public String ok() {
+        return "App B works";
     }
 
 }
