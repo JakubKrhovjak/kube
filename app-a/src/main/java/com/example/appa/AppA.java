a93ece35967fedc88221796ae20b241dea47998b
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2022-05-04 20:05:33 +0200

    dns resolution

--- app-a/src/main/java/com/example/appa/AppA.java
+++ app-a/src/main/java/com/example/appa/AppA.java
@@ -1,26 +1,31 @@
 package com.example.appa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
 @SpringBootApplication
 public class AppA {
 
-	@Autowired
-	private ItemRepository repository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppA.class, args);
 	}
 
+
+	@Value("${appb-url}")
+    private String appbUrl;
+
 	@GetMapping("/appa")
 	public String message() {
-		return  "Hello App A works! with value " + repository.findById(1L).get().getName();
+		RestTemplate restTemplate = new RestTemplate();
+		var b = restTemplate.getForObject(appbUrl + "/appb", String.class);
+		return  "Hello App A works! with value " + b;// + repository.findById(1L).get().getName();
 	}
 
 }
